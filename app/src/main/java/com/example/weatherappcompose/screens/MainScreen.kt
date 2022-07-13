15fdb35683ea0dc2a4d10ef46a5dfcffc75fed91
AlanKeepsCoding <Strateg9213@gmail.com>
2022-07-13 17:02:43 +0300

    add pager

--- app/src/main/java/com/example/weatherappcompose/screens/MainScreen.kt
+++ app/src/main/java/com/example/weatherappcompose/screens/MainScreen.kt
@@ -3,37 +3,43 @@ package com.example.weatherappcompose.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.weatherappcompose.R
+import com.example.weatherappcompose.ui.theme.BlueLight
+import com.example.weatherappcompose.ui.theme.Purple700
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
 import java.time.format.TextStyle
 
 
-@Preview (showBackground = true)
+@Preview(showBackground = true)
 @Composable
-fun MainScreen() {
-    Image(painter = painterResource(id = R.drawable.sky),
-        contentDescription = "im1", modifier = Modifier
-            .fillMaxSize()
-            .alpha(0.8f),
-        contentScale = ContentScale.Crop)
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(5.dp)
+fun MainCard() {
+    Column(
+        modifier = Modifier
+            .padding(5.dp)
     ) {
-        Card(modifier = Modifier.fillMaxWidth().alpha(0.5f),
-        backgroundColor = Color.Blue,
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            backgroundColor = BlueLight,
             elevation = 1.dp,
             shape = RoundedCornerShape(10.dp)
         ) {
@@ -42,7 +48,9 @@ fun MainScreen() {
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .alpha(0.5f),
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Text(
@@ -54,12 +62,116 @@ fun MainScreen() {
                     AsyncImage(
                         model = "https://cdn.weatherapi.com/weather/64x64/day/113.png",
                         contentDescription = "im2",
-                        modifier = Modifier.size(35.dp).
-                        padding(top = 3.dp, end = 8.dp)
+                        modifier = Modifier
+                            .size(35.dp)
+                            .padding(top = 3.dp, end = 8.dp)
                     )
                 }
+                Text(
+                    modifier = Modifier,
+                    text = "Kazan",
+                    style = androidx.compose.ui.text.TextStyle(fontSize = 24.sp),
+                    color = Color.White
+                )
+                Text(
+                    modifier = Modifier,
+                    text = "29°C",
+                    style = androidx.compose.ui.text.TextStyle(fontSize = 65.sp),
+                    color = Color.White
+                )
+                Text(
+                    modifier = Modifier,
+                    text = "Sunny",
+                    style = androidx.compose.ui.text.TextStyle(fontSize = 16.sp),
+                    color = Color.White
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    IconButton(
+                        onClick = {
 
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_search),
+                            contentDescription = "im3",
+                            tint = White
+                        )
+
+                    }
+                    Text(
+                        modifier = Modifier,
+                        text = "30°C/23°C",
+                        style = androidx.compose.ui.text.TextStyle(fontSize = 16.sp),
+                        color = Color.White
+                    )
+
+                    IconButton(
+                        onClick = {
+
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_sync),
+                            contentDescription = "im4",
+                            tint = White
+                        )
+
+                    }
+                }
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun TabLayout() {
+    val tabList = listOf("Hours", "Days")
+    val pagerState  = rememberPagerState()
+    val tabIndex = pagerState.currentPage
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier
+            .padding(
+                start = 5.dp,
+                end = 5.dp
+            )
+            .clip(RoundedCornerShape(5.dp))
+    ) {
+        TabRow(
+            selectedTabIndex = tabIndex,
+            indicator = {pos ->
+                        TabRowDefaults.Indicator(Modifier.pagerTabIndicatorOffset(pagerState, pos))
+            },
+            backgroundColor = BlueLight,
+            contentColor = Color.White
+        ) {
+            tabList.forEachIndexed { index, text ->
+                Tab(
+                    selected = false,
+                    onClick = {
+                              coroutineScope.launch {
+                                  pagerState.animateScrollToPage(index)
+                              }
+
+                    },
+                    text = {
+                        Text(text = text)
+                    }
+                )
+            }
+        }
+        HorizontalPager(
+            count = tabList.size,
+            state = pagerState,
+            modifier = Modifier.weight(1.0f)
+        ) {
+                index ->
+
         }
     }
 }
